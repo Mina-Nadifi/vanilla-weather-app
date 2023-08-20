c476ae726451ed93ef2375db710e374ebd84ad4d
minarnadifi <minasung8@gmail.com>
2023-08-20 07:22:38 +0330

    Finally added the refactored ,well described ultra unit converter , t converts all data between Imperial and metric system

--- src/script.js
+++ src/script.js
@@ -1,8 +1,19 @@
 function main() {
-  // Function #1 that uses axios response after the apiUrl is built
+  // Fetch Function called axiosGo
+  function axiosGo(city, units) {
+    let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+    let apiKey = "e450bc345a80a08ada69fd5c714d871d";
+    let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(showWeather);
+    axios.get(apiUrl).then(showDate);
+    axios.get(apiUrl).catch(function (error) {
+      alert("Please Enter a Valid City name");
+      document.querySelector("#locationInput").value = "";
+    });
+  }
+  // Function #1 that uses ---"Axios Response"--- after the apiUrl is built in anyway
   function showWeather(response) {
     document.querySelector("h1").innerHTML = response.data.name;
-
     document.querySelector(
       "#description"
     ).innerHTML = `${response.data.weather[0].description}`;
@@ -20,7 +31,7 @@ function main() {
     )}%`;
     document.querySelector(".windSpeed").innerHTML = `${Math.round(
       response.data.wind.speed
-    )} km/h`;
+    )}`;
     document.querySelector(".minTemp").innerHTML = ` ${Math.round(
       response.data.main.temp_min
     )}`;
@@ -29,7 +40,7 @@ function main() {
     )} `;
   }
 
-  // Setting Current Day and Time
+  // Setting Current Day and Time showDate function using ---"Axios Response"---
   function showDate(response) {
     let currentDate = new Date(response.data.dt * 1000);
     let weekDays = [
@@ -41,13 +52,18 @@ function main() {
       "Friday",
       "Saturday",
     ];
-    console.log(currentDate);
-    console.log("Hello world");
-    document.querySelector(".currentDate").innerHTML = `${
-      weekDays[currentDate.getDay()]
-    }    ${currentDate.getHours()}:${currentDate.getMinutes()}`;
+
+    if (currentDate.getMinutes() > 9) {
+      document.querySelector(".currentDate").innerHTML = `${
+        weekDays[currentDate.getDay()]
+      }    ${currentDate.getHours()}:${currentDate.getMinutes()}`;
+    } else {
+      document.querySelector(".currentDate").innerHTML = `${
+        weekDays[currentDate.getDay()]
+      }    ${currentDate.getHours()}:0${currentDate.getMinutes()}`;
+    }
   }
-  //Generated Api Url Using form location Input for city
+  //Generated Api Url Using form location Input for city ---  User Action
   document.querySelector(".inputCity").addEventListener("submit", (event) => {
     event.preventDefault();
     if (document.querySelector("#locationInput").value.length === 0) {
@@ -55,38 +71,72 @@ function main() {
     } else {
       cityName = document.querySelector("#locationInput").value;
     }
-    let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-    let apiKey = "e450bc345a80a08ada69fd5c714d871d";
-    let unit = "metric";
 
-    let apiUrl = `${apiEndpoint}?q=${cityName}&appid=${apiKey}&units=${unit}`;
-    axios.get(apiUrl).then(showWeather);
-    axios.get(apiUrl).then(showDate);
-    axios.get(apiUrl).catch(function (error) {
-      alert("Please Enter a Valid City name");
-      document.querySelector("#locationInput").value = "";
-    });
+    let unit = "metric";
+    axiosGo(cityName, unit);
   });
 
-  //1 --- Capitalizes First letter of every word
-  function capitalizer(name) {
-    const capitalized = name.charAt(0).toUpperCase() + name.slice(1);
-    return capitalized;
-  }
-
   //2 ---Default Api Url at page start
-  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-  let apiKey = "e450bc345a80a08ada69fd5c714d871d";
   let unit = "metric";
   let cityName = "New York";
 
   //4 ---Default Page Loading view
   document.querySelector("h1").innerHTML = cityName;
-  let apiUrl = `${apiEndpoint}?q=${cityName}&appid=${apiKey}&units=${unit}`;
-  axios.get(apiUrl).then(showWeather);
-  axios.get(apiUrl).then(showDate);
+  axiosGo(cityName, unit);
 
   //Unit change based on User's Request
-  document.querySelector(".foreign").addEventListener;
+  function classSwitcher(cl1, cl2) {
+    document.querySelectorAll(cl2)[0].classList.remove("linked");
+    document.querySelectorAll(cl2)[1].classList.remove("linked");
+    document.querySelectorAll(cl2)[2].classList.remove("linked");
+    document.querySelectorAll(cl1)[0].classList.add("linked");
+    document.querySelectorAll(cl1)[1].classList.add("linked");
+    document.querySelectorAll(cl1)[2].classList.add("linked");
+  }
+  function changeTempToFor() {
+    axiosGo(cityName, "imperial");
+    classSwitcher(".celcius", ".foreign");
+    document.querySelector(".speedUnit").innerHTML = "mp/h";
+  }
+
+  document
+    .querySelectorAll(".foreign")[0]
+    .addEventListener("click", changeTempToFor);
+  document
+    .querySelectorAll(".foreign")[1]
+    .addEventListener("click", changeTempToFor);
+  document
+    .querySelectorAll(".foreign")[2]
+    .addEventListener("click", changeTempToFor);
+
+  function changeTempToCel() {
+    axiosGo(cityName, "metric");
+    classSwitcher(".foreign", ".celcius");
+    document.querySelector(".speedUnit").innerHTML = "km/h";
+  }
+
+  document
+    .querySelectorAll(".celcius")[0]
+    .addEventListener("click", changeTempToCel);
+  document
+    .querySelectorAll(".celcius")[1]
+    .addEventListener("click", changeTempToCel);
+  document
+    .querySelectorAll(".celcius")[2]
+    .addEventListener("click", changeTempToCel);
+
+  /*
+  document.querySelectorAll(".linked").addEventListener("click", () => {
+    if (document.querySelector(".linked").innerHTML == " °F") {
+      axiosGo(cityName, "imperial");
+      document.querySelector(".celcius").classList.add("linked");
+      document.querySelector(".celcius").classList.remove("linked");
+    } else if (document.querySelector(".linked").innerHTML == "°C |") {
+      axiosGo(cityName, "metric");
+      document.querySelector(".celcius").classList.remove("linked");
+      document.querySelector(".foreign").classList.add("linked");
+    }
+  });*/
 }
+
 main();
